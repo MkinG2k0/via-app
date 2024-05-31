6467e98d71f3c89bc41852dc2d560e49eb4b672b
mk <kamai122000@mail.ru>
2024-06-01 00:24:33 +0300

    feat: lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,43 @@
-import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import {createHtmlPlugin} from 'vite-plugin-html';
-import fs from 'fs';
-import mkcert from 'vite-plugin-mkcert';
+import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
+import { defineConfig } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import mkcert from 'vite-plugin-mkcert'
 
-const hash = fs.readFileSync('public/definitions/hash.json', 'utf8');
+const hash = fs.readFileSync('public/definitions/hash.json', 'utf8')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    createHtmlPlugin({
-      inject: {
-        data: {
-          hash,
-        },
-      },
-    }),
-    mkcert(),
-  ],
-  assetsInclude: ['**/*.glb'],
-  envDir: '.',
-  server: {open: true,https:{}},
-  resolve: {
-    alias: {
-      src: path.resolve(__dirname, './src'),
-      assets: path.resolve(__dirname, './src/assets'),
-    },
-  },
-  optimizeDeps: {
-    include: ['@the-via/reader'],
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: 'globalThis',
-      },
-      // Enable esbuild polyfill plugins
-      plugins: [],
-    },
-  },
-});
+	plugins: [
+		react(),
+		createHtmlPlugin({
+			inject: {
+				data: {
+					hash,
+				},
+			},
+		}),
+		mkcert(),
+	],
+	assetsInclude: ['**/*.glb'],
+	envDir: '.',
+	server: { open: true, https: {} },
+	resolve: {
+		alias: {
+			src: path.resolve(__dirname, './src'),
+			assets: path.resolve(__dirname, './src/assets'),
+		},
+	},
+	optimizeDeps: {
+		include: ['@the-via/reader'],
+		esbuildOptions: {
+			// Node.js global to browser globalThis
+			define: {
+				global: 'globalThis',
+			},
+			// Enable esbuild polyfill plugins
+			plugins: [],
+		},
+	},
+})
